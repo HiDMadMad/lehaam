18b368f6c0603b433699d1aff1d01e37da1e4299
MadMad <151575623+HiDMadMad@users.noreply.github.com>
2025-07-18 19:03:19 +0330

    initial

---
+++ sleep_cycle_module.py
@@ -0,0 +1,19 @@
+ASCII_ART = """
+
+
+"""  # \\ are for fix the warning
+
+
+if __name__ == "__main__":
+    print(ASCII_ART,"use this file as a module!\n")
+
+
+MESSAGES = {}
+
+
+#================ classes ================#
+
+
+
+
+#================ functions ================#
